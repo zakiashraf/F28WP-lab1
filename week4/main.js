fbc56c12d155a9b35a7d6f83fb5f3eddddf7b56f
zakiashraf <145095617+zakiashraf@users.noreply.github.com>
2023-11-08 17:49:26 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,1 +1,6 @@
-
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://github.com/zakiashraf/F28WP-lab1/edit/main/week4/cities1.json');
+ourRequest.onload = function() {
+console.log(ourRequest.responseText);
+};
+ourRequest.send();
