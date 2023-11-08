be8e80e7d357b9a2a14c45246ecae2146a216730
zakiashraf <145095617+zakiashraf@users.noreply.github.com>
2023-11-08 18:12:08 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,7 +1,39 @@
+https://github.com/zakiashraf/F28WP-lab1/edit/main/week4/cities1.json
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://github.com/zakiashraf/F28WP-lab1/edit/main/week4/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-  console.log(ourData[0]);
+var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+btn.classList.add("hide-me");
 };
 ourRequest.send();
+});
+function renderHTML(data){
+var htmlString = "";
+for (i=0; i<data.length; i++){
+htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br>
+Where you can enjoy indoor places like: " ;
+for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+// Loop through the indoor places of the current city.
+if (ii == 0) {
+htmlString += data[i].places.indoor[ii];
+} else {
+htmlString += ", and " + data[i].places.indoor[ii];
+}
+}
+htmlString += '. & enjoy outdoor places like: ';
+// Loop through the outdoor places of the current city.
+for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+if (ii == 0) {
+htmlString += data[i].places.outdoor[ii];
+} else {
+htmlString += " and " + data[i].places.outdoor[ii];
+}
+}
+htmlString += '.</p>';
+}
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
